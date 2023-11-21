76b9b6a6e9f664aa2ce8e44476b09c647d323ded
sfmacdonald <seanfmacdonald@icloud.com>
2023-11-21 11:12:50 -0700

    Update README, added scripts & links for HTML & js

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # Day-Planner
-Employee Day Planner
+Plan your day because if you don't follow the 7 P's, it may lead to poor results.
+
+## Table of Contents
+
+- [Project Name](#project-name)
+- [Table of Contents](#table-of-contents)
+- [Description](#description)
+- [Features](#features)
+- [Usage](#usage)
+- [Testing](#testing)
+
+## Description
+
+The purpose of the website is to plan your day working at the company. The schedule shall display hour blocks with details regarding any scheduled meetings, etc. It is also color-coded to provide the user heightened visuals for determining availability.
+
+## Features
+
+This web-based scheduler has the following features and functions:
+
+Leverages jQuery and day.js integrations
+
+1. Display current date/time from user's local time
+    - Formatted as DD-MM-YYYY
+2. Change start time
+    - Nominal start time is 9AM
+3. Add new calendar entry with free text
+4. Save new calendar entry with save button on each time block
+5. Color coding for past, present and future blocks giving the user heightened visibility
+    - Gray = Past
+    - Red = Current
+    - Green = Future
+
+## Usage
+
+The github repository may be found at https://github.com/sfmacdonald/Day-Planner
+
+The live URL for the working website is [insert URL]
+
+When accessed, the website should reflect the following image:
+
+![alt text](<insert png>)
+
+## Testing
+
+1. **Navigate to the Website**
+   - Visit [insert URL]
+
+2. **Current Time/Date**
+    - Ensure that displayed date/time stamp matches user's date/time:
+        1. Date formatted as MM-DD-YYYY (24HRs)
+        2. Time formated as HH:mm:ss
+    - Rereshing browser shall provide updated date/time stamp
+
+3. **Time Blocks**
+    - Ensure that time blocks have correct color coding:
+        1. Current time - red
+        2. Past time(s) - gray 
+        3. Future time(s) - green
+    - Ensure that new entries save correctly by:
+        1. Click with cursor within time block the user wishes to create an entry
+            - Note blinking text cursor appears in selected time block 
+            - Note that the border of time block selected shall highlight
+        2. Insert free text
+        3. Select save button on right-hand side 
+            - Save button shall change color from white to black and enlarge when cursor is hovered over.
+
+
+
+
+4. **Completing The Quiz**
+    - 
+        1. 
+        2. 
+
+5. **Store Results**
+    - 
+        1. 
+        2. 
+        3. 
+
+6. **Begin A New Quiz**
+   - 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+$(document).ready(function() {
+
+// Select hours via jQuery
+var select = $("#hours");
+
+// Function to add hours to the dropdown using jQuery
+function addHours() {
+  for (var i = 0; i < 24; i++) {
+    var hour = (i < 10) ? "0" + i : i;
+    var ampm = (i < 12) ? "AM" : "PM";
+    var displayHour = (i % 12 === 0) ? "12:00 " + ampm : (i % 12) + ":00 " + ampm;
+    select.append($("<option>").val(hour).text(displayHour));
+  }
+}
+
+// Call the function to add hours - commented out for future state
+// addHours();
+
+// Get current date using Day.js
+var now = dayjs();
+console.log('Now:', now);
+
+// Function to display formatted date using jQuery
+function displayFormattedDate() {
+  // Format the date
+  var formattedDate = now.format("MM-DD-YYYY HH:mm:ss");
+  console.log("Formatted Date:", formattedDate);
+
+  // Update the content of the span element with the formatted date using jQuery
+  $("#formattedDateValue").text(formattedDate);
+}
+
+// Call the function to display formatted date
+displayFormattedDate();
+});
+
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -9,15 +43,74 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+
+ // Function to apply the past, present, or future class
+ function updateHourlyClasses() {
+  // Get the current hour using Day.js
+  const currentHour = dayjs().hour();
+
+  // Loop through each time block
+  const timeBlocks = document.querySelectorAll('.time-block');
+  timeBlocks.forEach((timeBlock) => {
+    // Extract the hour from the time block id
+    const blockHour = parseInt(timeBlock.id.split('-')[1]);
+
+    // Remove existing classes
+    timeBlock.classList.remove('past', 'present', 'future');
+
+    // Add the appropriate class based on the comparison
+    if (blockHour < currentHour) {
+      timeBlock.classList.add('past');
+    } else if (blockHour === currentHour) {
+      timeBlock.classList.add('present');
+    } else {
+      timeBlock.classList.add('future');
+    }
+  });
+}
+
+// Call the function on page load
+updateHourlyClasses();
+
+// Function to update time block classes based on the current hour
+function updateBlocks() {
+  var currentHour = new Date().getHours();
+
+  // Loop through each time block
+  document.querySelectorAll('.time-block').forEach(function (block) {
+      var blockHour = parseInt(timeBlock.id.split('-')[1]);
+
+      // Add the appropriate class based on the time
+      if (blockHour < currentHour) {
+          block.classList.add('past');
+      } else if (blockHour === currentHour) {
+          block.classList.add('present');
+      } else {
+          block.classList.add('future');
+      }
+  });
+}
+
+// Call the function when the page loads
+window.addEventListener('load', updateBlocks);
+
+
+
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  // Function to retrieve user input from local storage and set textarea values
+function retrieveAndSetUserInput() {
+  $('.time-block').each(function () {
+      var timeBlockId = $(this).attr('id');
+      var userInput = localStorage.getItem(timeBlockId);
+
+      // Set the textarea value with the retrieved user input
+      $(this).find('.description').val(userInput);
+  });
+}    
+
+
   });
